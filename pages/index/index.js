3baaea2c3ef91c915875820bb26046d078bd52be
tiger_yam <1641341031@qq.com>
2021-07-07 23:14:36 +0800

    create search swiper floor

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,46 @@
-wx-Page({
+//引入 用来发送请求的方法
+import {request} from "../../request/request.js";
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
-    
+    //轮播图数组
+    swiperList:[],
+    // 导航 数组
+    catesList: [],
+    //楼层 数组
+    floorList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  //页面开始加载 就会触发
+  onLoad: function(option){
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  // 获取 轮播图数据
+  getSwiperList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    .then(result => {
+      this.setData({
+        swiperList:result.data.message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  // 获取 分类导航数据
+  getCateList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
+    .then(result => {
+      this.setData({
+        catesList:result.data.message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  // 获取 楼层数据
+  getFloorList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"})
+    .then(result => {
+      this.setData({
+        floorList:result.data.message
+      })
+    })
   }
-})
+});
