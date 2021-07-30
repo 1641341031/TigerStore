19f6de67688aa6914006e50add14b1ce5fe9c690
tiger_yam <1641341031@qq.com>
2021-07-30 18:06:02 +0800

    create tabs-pages & goods_list-pages

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,7 @@
-// pages/goods_list/index.js
+import {request} from "../../request/request.js";
+import regeneratorRuntime from "../../lib/runtime/runtime";
+
+
 Page({
   data: {
     tabs: [
@@ -17,14 +20,42 @@ Page({
         value: "价格",
         isActive: false
       }
-    ]
+    ],
+    goodList: []
+  },
+
+  // 接口要的参数
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum:1,
+    pagesize:10
+  },
+
+  // 获取商品列表数据
+ async getGoodsList(){
+   const res = await request({url:"/goods/search", data:this.QueryParams});
+   this.setData({
+    goodList: res.goods
+   })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
-
+    this.QueryParams.cid=options.cid;
+    this.getGoodsList();
+  },
+  // 标题点击事件 从子组件传递过来
+  handleTabsItemChange(e){
+    // 获取被点击的标题索引
+    const {index} = e.detail;
+    // 修改源数组
+    let {tabs} = this.data;
+    tabs.forEach((v,i) => i===index?v.isActive=true:v.isActive=false);
+    this.setData({
+      tabs
+    })
   }
 })
